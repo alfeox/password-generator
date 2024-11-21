ddb3ebb6395c651bb1354c9f6fe0c41d4fac5300
Giu <mail@giualfeo.com>
2024-11-21 07:20:07 +0000

    added functionality to button, core tasks resolved

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let passwordField = document.getElementById("password1");
+let passwordField1 = document.getElementById("password1");
+let passwordField2 = document.getElementById("password2");
 
 function createPassword() {
     let pw = ""
@@ -14,7 +15,8 @@ function createPassword() {
 }
 
 function insertPassword() {
-    passwordField.textContent = createPassword();
+    passwordField1.textContent = createPassword();
+    passwordField2.textContent = createPassword();
 }
 
 
